f40984bcfd6d0024aebc088e974510674dad7af1
tkowalski <tkowalski@agtinternational.com>
2015-10-31 19:30:27 +0100

    initial commit - able to auto-detect numbers and dates and convert them automatically.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+exports.autoCoerce = function autoCoerce(obj) {
+  for(var key in obj) {
+    if (typeof obj[key] === 'object') {
+      obj[key] = fullCoerce( obj[key] );
+    }
+    else {        
+      obj[key] = coerceToDate( obj[key] );
+      obj[key] = coerceToNumber( obj[key] );
+    }
+  }
+  return obj;
+}
+
+function coerceToDate(text) {
+  try {
+    var date = new Date(text);
+    return date.toISOString() === text ? date : text ;
+  }
+  catch(ex) {
+    return text;
+  }
+}
+
+function coerceToNumber(text) {
+  var num = Number(text);
+  return isNaN(num) ? text : num;
+}
