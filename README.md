9641bba897710a33481d77fe30026e74a038ae6d
tkowalski <tkowalski@agtgermany.com>
2015-11-02 15:27:14 +0100

    Improved README.md

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # autocoerce
+[![GitHub version](https://badge.fury.io/gh/th-ko%2Fautocoerce.svg)](https://badge.fury.io/gh/th-ko%2Fautocoerce) [![npm version](https://badge.fury.io/js/autocoerce.svg)](https://badge.fury.io/js/autocoerce)
+
 A tiny library that automatically converts JSON fields to numbers or dates
+
+## Getting Started
+```
+var autocoerce = require('autocoerce');
+
+var myObj = {
+	someText: "Hello",
+	someNumber: "42",
+	someDate: "2014-12-31T23:00:00.000Z"
+}
+var newObj = autocoerce(myObj);
+=> {
+	someText: "Hello",
+	someNumber: 42,
+	someDate: new Date("2014-12-31T23:00:00.000Z")
+}
+```
+
+Optionally you can use a weaker date check which compares only the date part of the string
+```
+var autocoerce = require('autocoerce');
+
+var myObj = {
+	someText: "Hello",
+	someNumber: "42",
+	someDate: "2014-12-31"
+}
+var newObj = autocoerce(myObj, true);
+=> {
+	someText: "Hello",
+	someNumber: 42,
+	someDate: new Date("2014-12-31T00:00:00.000Z")
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
-module.exports = function autoCoerce(obj) {
+module.exports = function autoCoerce(obj, weakDateCheck) {
   for(var key in obj) {
     if (typeof obj[key] === 'object') {
-      obj[key] = fullCoerce( obj[key] );
+      obj[key] = autoCoerce( obj[key] );
     }
     else {        
-      obj[key] = coerceToDate( obj[key] );
-      obj[key] = coerceToNumber( obj[key] );
+      obj[key] = coerceISODate( obj[key], weakDateCheck );
+      obj[key] = coerceNumber( obj[key] );
     }
   }
   return obj;
 }
 
-function coerceToDate(text) {
+function coerceISODate(text, weakDateCheck) {
   try {
     var date = new Date(text);
-    return date.toISOString() === text ? date : text ;
+    if (weakDateCheck) {
+      return date.toISOString().slice(0,10) === text.slice(0,10) ? date : text ;
+    }
+    else {      
+      return date.toISOString() === text ? date : text ;
+    }
   }
   catch(ex) {
     return text;
   }
 }
 
-function coerceToNumber(text) {
+
+function coerceNumber(text) {
   var num = Number(text);
   return isNaN(num) ? text : num;
 }
